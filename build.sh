8e3f99f5955583558c1112ac0009755493f08601
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2021-07-22 15:26:39 -0700

    [docs] move files over to wonderland/croquet/docs

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+rm -rf ./build
+
+for i in `ls -d */| egrep -v '(build/|node_modules/|template/)'`
+do
+    (cd $i; npx jsdoc -c jsdoc.json -d ../build/$i)
+done
+
+cp index.html build/index.html
+
+VERSION="${VERSION:-0.5.0}"
+MINOR_VERSION="${MINOR_VERSION:-""}"
+
+echo $VERSION $MINOR_VERSION
+
+sed -i '' "s/@CROQUET_VERSION@/$VERSION/;s/@CROQUET_VERSION_MINOR@/$MINOR_VERSION/;" build/*/*.html || exit
