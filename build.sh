2d90afcc88de21cf836d8e35cfc1c99031ab9084
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2021-08-30 09:35:44 -0700

    [docs] allow different versions for individual packages

--- build.sh
+++ build.sh
@@ -8,12 +8,10 @@ rm -rf ./build/*
 
 for i in `ls -d */| egrep -v '(build/|node_modules/|template/)'`
 do
-    (cd $i; ../$JSDOC -c jsdoc.json -d ../build/$i)
+    VERSION=$(node -p -e "require('./"${i}package.json"').version")
+    echo $i $VERSION
+    (cd $i
+     ../$JSDOC -c jsdoc.json -d ../build/$i
+    )
+    sed -i '' "s/@CROQUET_VERSION@/$VERSION/;s/@CROQUET_VERSION_MINOR@/$MINOR_VERSION/;" build/${i}*.html || true
 done
-
-VERSION="${VERSION:-1.0.0}"
-MINOR_VERSION="${MINOR_VERSION:-""}"
-
-echo $VERSION $MINOR_VERSION
-
-sed -i '' "s/@CROQUET_VERSION@/$VERSION/;s/@CROQUET_VERSION_MINOR@/$MINOR_VERSION/;" build/*/*.html || exit
