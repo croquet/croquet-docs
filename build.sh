8cfe0064c762d7b5800443b33ac8d76aa692a372
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2021-09-03 15:13:07 -0700

    [docs] remove unnecessary build-win script

--- build.sh
+++ build.sh
@@ -9,8 +9,6 @@ for i in `ls -d */| egrep -v '(build/|node_modules/|template/)'`
 do
     VERSION=$(node -p -e "require('./"${i}package.json"').version")
     echo $i $VERSION
-    (cd $i
-      npm run build
-    )
+    (cd $i; npm run build)
     sed -i '' "s/@CROQUET_VERSION@/$VERSION/;s/@CROQUET_VERSION_MINOR@/$MINOR_VERSION/;" build/${i}*.html || true
 done
