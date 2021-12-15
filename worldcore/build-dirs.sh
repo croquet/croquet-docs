3b2028bf01b754489f4e88f3ee0398c9d4c64064
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2021-12-15 15:16:35 -0800

    [docs] add worldcore tutorials

--- worldcore/build-dirs.sh
+++ worldcore/build-dirs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for i in audio behavior input rapier three vector webGL widget
+for i in audio behavior input rapier three vector webgl widget
 do
     (cd $i; npx jsdoc -c jsdoc.json)
 done
