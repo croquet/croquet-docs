b56aa667e197ad1923b07baa05d2b12ad813ca32
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2022-06-19 15:33:19 -0700

    add the start of microverse jsdocs

---
+++ microverse/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true
+  },
+
+  'parserOptions': {
+      "ecmaVersion": 11,
+  },
+
+    rules: {
+        "no-empty-function": "off",
+        "no-unused-vars": "off",
+        "getter-return": "off",
+    }
+};
