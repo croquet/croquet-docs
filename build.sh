95f486ca0eecc4d41230d75d1835f77ff93b3f7e
Vanessa Freudenberg <vanessa@codefrau.net>
2024-03-10 17:06:30 -0700

    Download teatime docs if not building them (and we have wget)

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 cd `dirname "$0"`
 
+[ -x node_modules/.bin/jsdoc ] || npm ci
+
+rm -rf ./dist; mkdir ./dist
+
 PACKAGES="$@"
 if [ -z "$PACKAGES" ] ; then
     PACKAGES="virtual-dom worldcore microverse webshowcase"
@@ -8,6 +12,12 @@ if [ -z "$PACKAGES" ] ; then
         PACKAGES="croquet $PACKAGES"
     else
         echo "Not building teatime docs because source not present in ../wonderland/croquet/teatime"
+        if wget --version > /dev/null 2>&1 ; then
+            echo "    ...downloading teatime docs from croquet.io instead"
+            wget -q --show-progress -r -np -nH --cut-dirs 2 -P dist https://croquet.io/dev/docs/croquet/
+        else
+            echo "    ...not downloading teatime docs either because wget is not available"
+        fi
     fi
     if [ -d "../croquet-react/docs" ] ; then
         PACKAGES="$PACKAGES croquet-react"
@@ -15,10 +25,6 @@ if [ -z "$PACKAGES" ] ; then
         echo "Not building react docs because source not present in ../croquet-react/docs"
     fi
 fi
-
-[ -x node_modules/.bin/jsdoc ] || npm ci
-
-rm -rf ./dist; mkdir ./dist
 
 # right now images and styles go one directory above... but it is probably saner if it is kept
 # in docs
