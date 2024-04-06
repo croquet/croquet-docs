9cd2a57af5347181d0b2dff31b4dcc2040829c09
migmac99 <migmacthegamer@gmail.com>
2024-04-06 11:59:53 +0100

    wip savepoint

--- build.sh
+++ build.sh
@@ -5,42 +5,54 @@ cd `dirname "$0"`
 
 rm -rf ./dist; mkdir ./dist
 
-PACKAGES="$@"
-if [ -z "$PACKAGES" ] ; then
-    PACKAGES="virtual-dom worldcore microverse webshowcase"
-    if [ -d "../wonderland/croquet/teatime" ] ; then
-        PACKAGES="croquet $PACKAGES"
-    else
-        echo "Not building teatime docs because source not present in ../wonderland/croquet/teatime"
-        if wget --version > /dev/null 2>&1 ; then
-            echo "    ...downloading teatime docs from croquet.io instead"
-            wget -q --show-progress -r -np -nH --cut-dirs 2 -P dist https://croquet.io/dev/docs/croquet/
-        else
-            echo "    ...not downloading teatime docs either because wget is not available"
-        fi
-    fi
-    if [ -d "../croquet-react/docs" ] ; then
-        PACKAGES="$PACKAGES croquet-react"
-    else
-        echo "Not building react docs because source not present in ../croquet-react/docs"
-    fi
-    if [ -d "../croquet-for-unity-tutorials/docs" ] ; then
-        PACKAGES="$PACKAGES unity"
-    else
-        echo "Not building unity docs because source not present in ../croquet-for-unity-tutorials/docs"
-    fi
-fi
+# PACKAGES="$@"
+# if [ -z "$PACKAGES" ] ; then
+#     PACKAGES="virtual-dom worldcore microverse webshowcase"
+#     if [ -d "../wonderland/croquet/teatime" ] ; then
+#         PACKAGES="croquet $PACKAGES"
+#     else
+#         echo "Not building teatime docs because source not present in ../wonderland/croquet/teatime"
+#         if wget --version > /dev/null 2>&1 ; then
+#             echo "    ...downloading teatime docs from croquet.io instead"
+#             wget -q --show-progress -r -np -nH --cut-dirs 2 -P dist https://croquet.io/dev/docs/croquet/
+#         else
+#             echo "    ...not downloading teatime docs either because wget is not available"
+#         fi
+#     fi
+#     if [ -d "../croquet-react/docs" ] ; then
+#         PACKAGES="$PACKAGES croquet-react"
+#     else
+#         echo "Not building react docs because source not present in ../croquet-react/docs"
+#     fi
+#     if [ -d "../croquet-for-unity-tutorials/docs" ] ; then
+#         PACKAGES="$PACKAGES unity"
+#     else
+#         echo "Not building unity docs because source not present in ../croquet-for-unity-tutorials/docs"
+#     fi
+# fi
 
 # right now images and styles go one directory above... but it is probably saner if it is kept
 # in docs
-cp -rp ./clean-jsdoc-theme/static/ ./dist/
+cp -rp ./clean-jsdoc-theme/static/ ./dist/croquet
+cp -rp ./jsdoc-theme/static/ ./dist/multisynq
 
-for i in $PACKAGES
+PACKAGES="croquet-react"
+TARGETS="croquet multisynq"
+
+for t in ${TARGETS}
 do
-    VERSION=$(node -p -e "require('./"${i}/package.json"').version")
-    MINOR_VERSION=`echo $VERSION | sed 's/\.[^.]*$//'`
-    echo $i $VERSION
-    (cd $i; npm run build) || exit 1
-    [ -f "dist/${i}/index.html" ] || exit 1
-    sed -i '' "s/@CROQUET_VERSION@/$VERSION/;s/@CROQUET_VERSION_MINOR@/$MINOR_VERSION/;" dist/${i}/*.html || true
+    mkdir dist/${t}
+    for p in $PACKAGES
+    do
+        output="dist/${t}/${p}"
+        echo ${t} ${p} ${output}
+        VERSION=$(node -p -e "require('./"${p}/package.json"').version")
+        MINOR_VERSION=`echo $VERSION | sed 's/\.[^.]*$//'`
+        echo $p $VERSION
+        echo ${output} ${VERSION}
+        (cd $p; npm run build -- --template ../themes/${t}) || exit 1
+        # mv ${p}/out/ ${output}
+        [ -f "${output}/index.html" ] || exit 1
+        # sed -i '' "s/@CROQUET_VERSION@/$VERSION/;s/@CROQUET_VERSION_MINOR@/$MINOR_VERSION/;" ${output}/*.html || true
+    done
 done
---
+++ multisynq-react/README.md
@@ -0,0 +1,13 @@
+# Multisynq-React Docs
+
+The actual docs are in the croquet-react repo now: https://github.com/croquet/croquet-react/
+
+This repo has the doc generator and theme. It expects `croquet-react` to be checked out next to `croquet-docs`.
+
+
+    ├── croquet-react
+    │   └── docs
+    │
+    └── croquet-docs
+
+If that's in place, you can build the react docs like any other using `npm run build` or `npm run watch` in this directory.
