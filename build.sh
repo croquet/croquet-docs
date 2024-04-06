c0d77711bf8c48f032b8153b4aee801203a34734
afonsocrg <afonso.crg@gmail.com>
2024-04-06 15:04:39 +0100

    uncomment packages builder; fixed croquet/jsdoc.json

--- build.sh
+++ build.sh
@@ -1,47 +1,54 @@
 #!/bin/sh
+
+set -euo pipefail
+
 cd `dirname "$0"`
 
 [ -x node_modules/.bin/jsdoc ] || npm ci
 
 rm -rf ./dist; mkdir ./dist
 
-# PACKAGES="$@"
-# if [ -z "$PACKAGES" ] ; then
-#     PACKAGES="virtual-dom worldcore microverse webshowcase"
-#     if [ -d "../wonderland/croquet/teatime" ] ; then
-#         PACKAGES="croquet $PACKAGES"
-#     else
-#         echo "Not building teatime docs because source not present in ../wonderland/croquet/teatime"
-#         if wget --version > /dev/null 2>&1 ; then
-#             echo "    ...downloading teatime docs from croquet.io instead"
-#             wget -q --show-progress -r -np -nH --cut-dirs 2 -P dist https://croquet.io/dev/docs/croquet/
-#         else
-#             echo "    ...not downloading teatime docs either because wget is not available"
-#         fi
-#     fi
-#     if [ -d "../croquet-react/docs" ] ; then
-#         PACKAGES="$PACKAGES croquet-react"
-#     else
-#         echo "Not building react docs because source not present in ../croquet-react/docs"
-#     fi
-#     if [ -d "../croquet-for-unity-tutorials/docs" ] ; then
-#         PACKAGES="$PACKAGES unity"
-#     else
-#         echo "Not building unity docs because source not present in ../croquet-for-unity-tutorials/docs"
-#     fi
-# fi
+PACKAGES="$@"
+if [ -z "$PACKAGES" ] ; then
+    PACKAGES="virtual-dom worldcore microverse webshowcase"
+    if [ -d "../wonderland/croquet/teatime" ] ; then
+        PACKAGES="croquet $PACKAGES"
+    else
+        echo "Not building teatime docs because source not present in ../wonderland/croquet/teatime"
+        
+        if wget --version > /dev/null 2>&1 ; then
+            echo "    ...downloading teatime docs from croquet.io instead"
+            # Here we should make sure if we are downloading for wanted targets
+            wget -q --show-progress -r -np -nH --cut-dirs 2 -P dist/croquet https://croquet.io/dev/docs/croquet/ || true
+            # wget -q --show-progress -r -np -nH --cut-dirs 2 -P dist/multisynq https://multisynq.io/dev/docs/multisynq/
+        else
+            echo "    ...not downloading teatime docs either because wget is not available"
+        fi
+    fi
+
+    if [ -d "../croquet-react/docs" ] ; then
+        PACKAGES="$PACKAGES croquet-react"
+    else
+        echo "Not building react docs because source not present in ../croquet-react/docs"
+    fi
+
+    if [ -d "../croquet-for-unity-tutorials/docs" ] ; then
+        PACKAGES="$PACKAGES unity"
+    else
+        echo "Not building unity docs because source not present in ../croquet-for-unity-tutorials/docs"
+    fi
+fi
 
 # right now images and styles go one directory above... but it is probably saner if it is kept
 # in docs
 
-PACKAGES="croquet-react"
 TARGETS="croquet multisynq"
 
 for t in ${TARGETS}
 do
     TEMPLATE_DIR=templates/${t}
 
-    mkdir dist/${t}
+    mkdir -p dist/${t}
     # TODO: use ln instead of cp
     cp -rp ./${TEMPLATE_DIR}/static/* ./dist/${t}
     for p in $PACKAGES
