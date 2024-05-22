e9320e59eb1b705b14f97e9b49a1734a1d2be607
afonsocrg <afonso.crg@gmail.com>
2024-05-22 09:56:49 +0100

    rename webGL -> webgl

--- worldcore/build.sh
+++ worldcore/build.sh
@@ -20,7 +20,7 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-for i in audio behavior input rapier three vector webGL widget
+for i in audio behavior input rapier three vector webgl widget
 do
     (cd $i; npx jsdoc -c jsdoc.json --template ${template} --destination ${destination}/${i})
 done
